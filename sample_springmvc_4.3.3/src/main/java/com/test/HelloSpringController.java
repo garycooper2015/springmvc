2526ca6190cada512005e3aa52c02c0bac8d1f63
gayrcooper <garycooper@126.com>
2017-12-01 16:26:28 +0800

    commit1

---
+++ sample_springmvc_4.3.3/src/main/java/com/test/HelloSpringController.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloSpringController {
+    String message = "Welcome to Spring MVC!";
+ 
+    @RequestMapping("/hello")
+    public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "Spring") String name) {
+ 
+        ModelAndView mv = new ModelAndView("hellospring");//指定视图
+ 
+        /*
+         * 比如这是业务层，得到业务的数据
+         */
+        mv.addObject("name", name);
+        mv.addObject("message",message);
+        
+        /*
+         * 实现把集合数据传递到页面上
+         */
+        List<String> list1 = new ArrayList<String>();
+        list1.add("张三");
+        list1.add("李四");
+        list1.add("王五");
+        
+        mv.addObject("list1", list1);
+        
+        return mv;
+    }
+}	
---
+++ sample_springmvc_4.3.3/target/sample_springmvc_4.3.3/WEB-INF/view/hellospring.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Spring 4 MVC -HelloWorld</title>
+<style>
+	body{
+		background-color:#f7f7f9;
+	}
+</style>
+</head>
+<body>
+    <center>
+        <h2>Hello World</h2>
+        <h2>
+            ${message} ${name} </h2>
+            
+        <c:forEach var="item" items="${list1}">
+        		${item}<br/>
+        </c:forEach>
+            
+    </center>
+</body>
+</html>
